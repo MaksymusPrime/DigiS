17f046377480891a7aac5c01655485a6be7a16c5
MaksymusPrime <grafdemaxo@gmail.com>
2023-05-24 00:04:18 +0300

    media/favicon

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 let darkmode = document.querySelector('#darkmode');
 
 darkmode.onclick = () => {
-    if (darkmode.classList.contains('bx-moon')) {
-        darkmode.classList.replace ('bx-moon', 'bx-sun');
+    if (darkmode.classList.contains('bxs-moon')) {
+        darkmode.classList.replace ('bxs-moon', 'bxs-sun');
         document.body.classList.add('color');
+        navlist.classList.add('color');
     }else{
-        darkmode.classList.replace ('bx-sun', 'bx-moon');
+        darkmode.classList.replace ('bxs-sun', 'bxs-moon');
         document.body.classList.remove('color');
+        navlist.classList.remove('color');
     }
 }
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+const sr = ScrollReveal ({
+    distance: '70px',
+    duration: 2700,
+    reset: true
+})
+
+sr.reveal('.hero-text',{delay:200, origin: 'bottom'});
+sr.reveal('.hero-img',{delay:350, origin: 'top'});
+sr.reveal('.down-arrow',{delay:450, origin: 'right'});
